edaa4f8b2e38a050c6eb6e99ac897c25e749b1b6
Sebastian Köln <sebk@rynx.org>
2017-08-31 14:44:26 +0200

    #[inline] more

--- src/m_init.rs
+++ src/m_init.rs
@@ -1,6 +1,7 @@
 macro_rules! m_init {
     ($($Tuple:ident { $($T:ident . $t:ident . $idx:tt),* } )*) => ($(
         impl<$($T),*> Clone for $Tuple<$($T),*> where $( $T: Clone ),* {
+            #[inline(always)]
             fn clone(&self) -> Self {
                 $Tuple( $( self.$idx.clone() ),* )
             }
@@ -8,6 +9,7 @@ macro_rules! m_init {
         impl<$($T),*> Copy for $Tuple<$($T),*> where $( $T: Copy ),* {}
         
         impl<$($T),*> PartialEq for $Tuple<$($T),*> where $( $T: PartialEq ),* {
+            #[inline(always)]
             fn eq(&self, other: &Self) -> bool {
                 $( self.$idx == other.$idx)&&*
             }
@@ -21,18 +23,21 @@ macro_rules! m_init {
             }
         }
         impl<$($T),*> Default for $Tuple<$($T),*> where $( $T: Default ),* {
+            #[inline(always)]
             fn default() -> Self {
                 $Tuple( $( $T::default() ),* )
             }
         }
         impl<$($T),*> From<u16> for $Tuple<$($T),*> where $( $T: From<u16> ),* {
+            #[inline(always)]
             fn from(value: u16) -> Self {
                 $Tuple( $( $T::from(value) ),* )
             }
         }
         impl<$($T),*> Iterator for $Tuple<$($T),*> where $( $T: Iterator  ),* {
             type Item = $Tuple<$($T::Item),*>;
-            #[allow(non_snake_case)] 
+            #[allow(non_snake_case)]
+            #[inline(always)]
             fn next(&mut self) -> Option<Self::Item> {
                 match ( $(self.$idx.next(), )* ) {
                     ( $( Some($T) ,)* ) => Some($Tuple( $($T),* )),
